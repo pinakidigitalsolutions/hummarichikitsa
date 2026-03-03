96d92f1f976aa230a4d602fafc057e3e571902ce
Shivam-SG <shivam70410@gmail.com>
2026-03-03 20:09:43 +0530

    cors updated

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   // origin: "http://localhost:5173",
-  origin: "http://localhost:5173",
+  // origin: "http://localhost:5173",
+  origin: "https://hummarichikitsa.vercel.app",
   credentials: true
 }));
 
